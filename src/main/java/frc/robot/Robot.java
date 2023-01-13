119a84de739a79b98913711fa42fc3b9f171cb75
Abby Sliwinski <24abisli@student.cfschools.org>
2023-01-12 19:45:57 -0600

    Add Initial Project

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,51 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot 
+{
+    private Command _autonomousCommand;
+
+    private RobotContainer _robotContainer;
+
+    @Override
+    public void robotInit() 
+    {
+        _robotContainer = new RobotContainer();
+    }
+
+    @Override
+    public void robotPeriodic() 
+    {
+        CommandScheduler.getInstance().run();
+    }
+
+  
+    @Override
+    public void autonomousInit() 
+    {
+        _autonomousCommand = _robotContainer.getAutonomousCommand();
+
+        if (_autonomousCommand != null) 
+        {
+            _autonomousCommand.schedule();
+        }
+    }
+
+    @Override
+    public void teleopInit() 
+    {
+        if (_autonomousCommand != null) 
+        {
+            _autonomousCommand.cancel();
+        }
+    }
+
+    @Override
+    public void testInit() 
+    {
+        CommandScheduler.getInstance().cancelAll();
+    }
+}
