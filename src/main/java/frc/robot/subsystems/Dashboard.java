26b56aac7ce0dcb267a666387470eb33e9fb7860
Eden Reicherts <edenreicherts@gmail.com>
2023-01-14 15:44:49 -0600

    Addede writing values

--- src/main/java/frc/robot/subsystems/Dashboard.java
+++ src/main/java/frc/robot/subsystems/Dashboard.java
@@ -9,15 +9,19 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Dashboard extends SubsystemBase 
 {
     private GenericEntry _y;
+    private GenericEntry _x;
     public Dashboard() 
     {
         var x = Shuffleboard.getTab("Dashboard");
         _y = x.add("number", 0).getEntry();
+        _x = x.add("number2", 0).getEntry();
     }
 
     @Override
     public void periodic() 
     {
-     System.out.println(_y.getDouble(0));
+        double x = (_y.getDouble(0));
+        System.out.println(x);
+        _x.setDouble(x);
     }
 }
