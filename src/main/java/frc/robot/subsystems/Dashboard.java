98e96712bea5bb075e837d5e99a46117817fae9c
Eden Reicherts <edenreicherts@gmail.com>
2023-01-14 15:55:38 -0600

    Added button example

--- src/main/java/frc/robot/subsystems/Dashboard.java
+++ src/main/java/frc/robot/subsystems/Dashboard.java
@@ -2,26 +2,29 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Dashboard extends SubsystemBase 
 {
-    private GenericEntry _y;
     private GenericEntry _x;
     public Dashboard() 
     {
         var x = Shuffleboard.getTab("Dashboard");
-        _y = x.add("number", 0).getEntry();
-        _x = x.add("number2", 0).getEntry();
+        _x = x.add("button", false).withWidget(BuiltInWidgets.kToggleButton).getEntry();
     }
 
     @Override
     public void periodic() 
     {
-        double x = (_y.getDouble(0));
-        System.out.println(x);
-        _x.setDouble(x);
+        boolean x = (_x.getBoolean(false));
+        
+        if (x)
+        {
+         System.out.println("Pressed");
+         _x.setBoolean(false);
+        }
     }
 }
