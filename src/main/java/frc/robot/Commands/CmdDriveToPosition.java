fb2e5522787fbd5c36d5b74e0bd1d230db4f2945
Eden Reicherts <edenreicherts@gmail.com>
2023-01-21 23:31:05 -0600

    Added command folder and CmdDriveToPosition

---
+++ src/main/java/frc/robot/Commands/CmdDriveToPosition.java
@@ -0,0 +1,35 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class CmdDriveToPosition extends CommandBase 
+{
+    public CmdDriveToPosition() 
+    {
+        
+    }
+
+    @Override
+    public void initialize() 
+    {
+
+    }
+
+    @Override
+    public void execute() 
+    {
+
+    }
+
+    @Override
+    public void end(boolean interrupted) 
+    {
+
+    }
+
+    @Override
+    public boolean isFinished() 
+    {
+        return false;
+    }
+}
