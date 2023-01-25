de4f30d8edb57b68dd9837733ec49ad917fa4b4b
aGenius21 <25isahut@student.cfschools.org>
2023-01-24 20:34:25 -0600

    We can now pull skew yaw pitch for targets 0 and 1

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -47,14 +47,7 @@ public class Vision extends SubsystemBase
     {
         return _latestResult0.hasTargets();
     }
-
-    public Transform2d getTargetPosition()
-    {
-        Transform3d transformToTarget = _latestResult0.getBestTarget().getBestCameraToTarget();
-
-        return new Transform2d();
-    }
-
+    
   @Override
     public void periodic()
     {
@@ -82,13 +75,44 @@ public class Vision extends SubsystemBase
 
         return aprilTagFieldLayout;
     }
-    public Pose3d robotPose0()
+    public double skew0()
     {
         PhotonTrackedTarget target0 = _latestResult0.getBestTarget();
-        Transform3d cameraToRobot0 = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0));
-        Pose3d robotPose0 = PhotonUtils.estimateFieldToRobotAprilTag(target0.getBestCameraToTarget(), aprilTagFieldLayout().getTagPose(target0.getFiducialId()).get(), cameraToRobot0);
-        return robotPose0;
+        double skew0 = target0.getSkew();
+        return skew0;
     }
+    public double yaw0()
+    {
+        PhotonTrackedTarget target0 = _latestResult0.getBestTarget();
+        double yaw0 = target0.getYaw();
+        return yaw0;
+    }
+    public double pitch0()
+    {
+        PhotonTrackedTarget target0 = _latestResult0.getBestTarget();
+        double pitch0 = target0.getPitch();
+        return pitch0;
+    }
+    
+    public double skew1()
+    {
+        PhotonTrackedTarget target1 = _latestResult1.getBestTarget();
+        double skew1 = target1.getSkew();
+        return skew1;
+    }
+    public double yaw1()
+    {
+        PhotonTrackedTarget target1 = _latestResult1.getBestTarget();
+        double yaw1 = target1.getYaw();
+        return yaw1;
+    }
+    public double pitch1()
+    {
+        PhotonTrackedTarget target1 = _latestResult1.getBestTarget();
+        double pitch1 = target1.getPitch();
+        return pitch1;
+    }
+    
     public Pose3d robotPose1()
     {
         PhotonTrackedTarget target1 = _latestResult1.getBestTarget();
@@ -96,6 +120,8 @@ public class Vision extends SubsystemBase
         Pose3d robotPose1 = PhotonUtils.estimateFieldToRobotAprilTag(target1.getBestCameraToTarget(), aprilTagFieldLayout().getTagPose(target1.getFiducialId()).get(), cameraToRobot1);
         return robotPose1;
     }
+
+
     public void targets()
     {
         PhotonTrackedTarget target0 = _latestResult0.getBestTarget();
