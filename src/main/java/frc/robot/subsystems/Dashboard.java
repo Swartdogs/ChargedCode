09ee68793233f8284712d0a034cc2df636d01f31
Eden Reicherts <edenreicherts@gmail.com>
2023-01-25 23:27:05 -0600

    Changed order of .withPosition and .withSize

--- src/main/java/frc/robot/subsystems/Dashboard.java
+++ src/main/java/frc/robot/subsystems/Dashboard.java
@@ -22,10 +22,10 @@ public class Dashboard extends SubsystemBase
         var allianceBox = tab.add("allianceColorBox", false).withPosition(0, 0).withSize(28, 1).withWidget(BuiltInWidgets.kBooleanBox);
         var visionLayout = tab.getLayout("VisionBox", BuiltInLayouts.kGrid).withPosition(0, 2).withSize(8, 2);
         var hasTargetBox = visionLayout.add("hasTargetBox", false).withPosition(0, 0).withSize(4, 1).withWidget(BuiltInWidgets.kBooleanBox);
-        var targetID = visionLayout.add("TargetID", 0).withSize(4, 1).withPosition(4, 0).withWidget(BuiltInWidgets.kTextView);
+        var targetID = visionLayout.add("TargetID", 0).withPosition(4, 0).withSize(4, 1).withWidget(BuiltInWidgets.kTextView);
         var driveBaseLayout = tab.getLayout("DriveBaseLayout", BuiltInLayouts.kGrid).withPosition(0, 5).withSize(4, 4);
         //var heading = driveBaseLayout.add("Heading", 0).withProperties(Map.of("Min", -2, "Max", 12)).withSize(4, 1).withPosition(0, 0).withWidget(BuiltInWidgets.kNumberBar);
-        var heading = driveBaseLayout.add("Heading", 0).withSize(4, 1).withPosition(0, 0).withWidget(BuiltInWidgets.kTextView);
+        var heading = driveBaseLayout.add("Heading", 0).withPosition(0, 0).withSize(4, 1).withWidget(BuiltInWidgets.kTextView);
         var swerveAnglesLayout = tab.getLayout("SwerveAngles", BuiltInLayouts.kGrid).withPosition(4, 5).withSize(4, 4);
         var frAngle = swerveAnglesLayout.add("FR", 0).withPosition(2, 0).withSize(2,2).withWidget(BuiltInWidgets.kTextView);
         var flAngle = swerveAnglesLayout.add("FL", 0).withPosition(0, 0).withSize(2,2).withWidget(BuiltInWidgets.kTextView);
