63075beb66b23d5ffbce44ecb72ebfff29a9e360
Pandaroo <60902862+JoeKangas@users.noreply.github.com>
2023-02-10 18:42:21 -0600

    Driver controls (#17)
    * Fixed merge conflict
    
    * removed button 1
    
    * added cancel command
    
    ---------
    
    Co-authored-by: Collin McIntyre <crm11396@gmail.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,11 +3,10 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-
-import frc.robot.commands.CmdDriveWithJoystick;
 import frc.robot.commands.CmdAutoRotate;
-//import frc.robot.commands.CmdDriveBalance;
+import frc.robot.commands.CmdDriveWithJoystick;
 import frc.robot.commands.CmdDriveResetEncoders;
 import frc.robot.commands.CmdDriveResetOdometer;
 import frc.robot.commands.CmdDriveToPosition;
@@ -17,21 +16,23 @@ import frc.robot.subsystems.drive.Vector;
 
 public class RobotContainer 
 {
-    private Joystick _driveJoy;
-    private JoystickButton _driveJoyButton1;
+    private Joystick       _driveJoy;
     private JoystickButton _driveJoyButton2;
     private JoystickButton _driveJoyButton3;
     //private JoystickButton _driveJoyButton6;
+    private JoystickButton _driveJoyButton9;
     private JoystickButton _driveJoyButton11;
+
+    private JoystickButton _driveJoyButton7;
 
     public RobotContainer()
     {
-        _driveJoy = new Joystick(0);
+        _driveJoy         = new Joystick(0);
 
-        _driveJoyButton1 = new JoystickButton(_driveJoy, 1);
-        _driveJoyButton2 = new JoystickButton(_driveJoy, 2);
-        _driveJoyButton3 = new JoystickButton(_driveJoy, 3);
+        _driveJoyButton2  = new JoystickButton(_driveJoy, 2);
+        _driveJoyButton3  = new JoystickButton(_driveJoy, 3);
         //_driveJoyButton6 = new JoystickButton(_driveJoy, 6);
+        _driveJoyButton9  = new JoystickButton(_driveJoy, 9);
         _driveJoyButton11 = new JoystickButton(_driveJoy, 11);
         
         Dashboard.getInstance();
@@ -46,29 +47,32 @@ public class RobotContainer
         (
             new CmdDriveWithJoystick
             (
-                () -> getJoystickAxis(_driveJoy.getX(), false, false, 0.05), 
-                () -> getJoystickAxis(_driveJoy.getY(), true,  false, 0.05), 
-                () -> getJoystickAxis(_driveJoy.getZ(), false, true,  0.10), 
-                () -> _driveJoy.getRawButton(1)
+                this::getDriveJoyX,
+                this::getDriveJoyY,
+                this::getDriveJoyZ,
+                this::driveIsRobotCentric
             )  
         );
     }
 
     private void configureBindings() 
     {
-        _driveJoyButton1.onTrue
-        (
-            new CmdAutoRotate
-            (
-                90,  
-                () -> getJoystickAxis(_driveJoy.getX(), false, false, 0.05), 
-                () -> getJoystickAxis(_driveJoy.getY(), true,  false, 0.05),  
-                () -> _driveJoy.getRawButton(1)
-            )
-        );
+        CmdDriveToPosition driveToPosition   = new CmdDriveToPosition(new Vector(), 0);
+        CmdAutoRotate      driveAutoRotate90 = new CmdAutoRotate(90, this::getDriveJoyX, this::getDriveJoyY, this::driveIsRobotCentric);
+        //CmdDriveBalance    driveAutoBalance  = new CmdDriveBalance();
+
         _driveJoyButton2.onTrue(new CmdDriveResetOdometer());
-        _driveJoyButton3.onTrue(new CmdDriveToPosition(new Vector(), 0));
+        _driveJoyButton3.onTrue(driveToPosition);
         //_driveJoyButton6.onTrue(new CmdDriveBalance());
+        _driveJoyButton7.onTrue(driveAutoRotate90);
+        _driveJoyButton9.onTrue
+        (
+            new InstantCommand(() -> 
+            {
+                driveToPosition.cancel();
+                driveAutoRotate90.cancel();
+            })
+        );
         _driveJoyButton11.onTrue(new CmdDriveResetEncoders());
     }
 
@@ -102,5 +106,25 @@ public class RobotContainer
         }
 
         return applyDeadband(input, deadband);
+    }
+
+    private double getDriveJoyX()
+    {
+        return getJoystickAxis(_driveJoy.getX(), false, false, 0.05);
+    }
+
+    private double getDriveJoyY()
+    {
+        return getJoystickAxis(_driveJoy.getY(), true, false, 0.05);
+    }
+
+    private double getDriveJoyZ()
+    {
+        return getJoystickAxis(_driveJoy.getZ(), false, true, 0.1);
+    }
+
+    private boolean driveIsRobotCentric()
+    {
+        return _driveJoy.getRawButton(1);
     }
 }
